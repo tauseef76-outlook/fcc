e06a2276c0a6e0ed9d01f23d1a6d92b137d24c83
Tauseef <tauseef76@outlook.com>
2020-02-23 06:58:46 +0530

    first commit

---
+++ form/src/App.js
@@ -0,0 +1,68 @@
+import React, {Component} from 'react';
+import './App.css';
+
+class App extends Component{
+  constructor(props){
+    super(props);
+
+    this.state = {
+      newItem:"",
+      list: []
+    }
+  }
+
+  updateInput(key,value){
+    this.setState({
+      [key]:value,
+    });
+  }
+
+  addItem(){
+    const newItem = {
+      id: 1+Math.random(),
+      value: this.state.newItem.slice()
+    }
+
+    const list = [...this.state.list];
+
+    list.push(newItem);
+
+    this.setState({
+      list,
+      newItem: ""
+    });
+  }
+
+  deleteItem(id){
+    const list = [...this.state.list];
+
+    const updatedList = list.filter(item => item.id !== id);
+    this.setState({list: updatedList});
+  }
+
+  render(){
+    return(
+      <div className = "App">
+        <div>
+          Add an Item....
+        <br />
+        <input type="text" value={this.state.newItem} onChange={e => this.updateInput("newItem", e.target.value
+        )} />
+    </div>
+      <button onClick={() => this.addItem()} > Add </button>
+      <br />
+      <ul>
+        {this.state.list.map(item => {
+          return(
+            <li key = {item.id}>
+              {item.value}
+              <button onClick={() => this.deleteItem(item.id)}>X</button>
+            </li>
+          )
+        })}
+      </ul>
+        </div>
+    );
+  }
+}
+export default App;
